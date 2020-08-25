d0a901dc6f8d17b2b013e4cc462f8be67194f369
ab800 <64869646+ab800@users.noreply.github.com>
2020-08-25 01:58:10 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -26,11 +26,11 @@ class FixUnderfit:
     
     def transform(self, X):
         # Primero copiamos el dataframe de datos de entrada 'X'
-        df = X.copy()
+        df2 = X.copy()
             
         #divido dataframes uno con Sospechosos, otro con Aceptados
-        df4 = df.loc[df['OBJETIVO'] == 'Sospechoso']
-        df5= df.loc[df['OBJETIVO'] == 'Aceptado']
+        df4 = df2.loc[df2['OBJETIVO'] == 'Sospechoso']
+        df5= df2.loc[df2['OBJETIVO'] == 'Aceptado']
 
         #para balancear muestras tomo 8000 del df con sospechosos
         df8 = df4.sample(8000, replace=True)
